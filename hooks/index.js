ce5c60e1d0ef1b45ee0691810ecb89a13b018a5b
DeepanshuYadav-code <yadavdeepanshu000@gmail.com>
2025-04-08 03:15:32 +0530

    removed basic instructions

--- hooks/index.js
+++ hooks/index.js
@@ -1,7 +1,3 @@
-// This file is a placeholder for customization hooks.
-// Hooks allow you to modify the AsyncAPI document or the generated files dynamically.
-// For example, you can add custom logic to transform the AsyncAPI document before rendering templates.
-
 module.exports = {
   // Example hook: Modify the AsyncAPI document before generation
   'generate:before': (generator) => {
